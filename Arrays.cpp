82a11c89b0bd7513dfdac65ddf97f9359670c9a2
Taha Mehboob <110561252+Taha028@users.noreply.github.com>
2023-02-14 02:31:58 +0500

    Add files via upload

---
+++ Arrays.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+using namespace std;
+
+// void createanddisplay(int size){
+//     int arr[size];
+//     string k;
+//     for(int i=0;i<size;i++){
+//         cin>>arr[i];
+//     }
+//     cout<<"Enter To Show Array or Not";
+//     cin>>k;
+//     if (k=="show"){
+//     cout<<"The Elements are";
+//     for(int i=0;i<size;i++){
+//         cout<<arr[i]<<" ";
+//     }
+//     cout<<endl;
+//     }
+//     else if (k=="No"){ 
+//         cout<<" ";
+//     }
+// }
+// int main() {
+//     int size;
+//     cout<<"Enter The size of Array";
+//     cin>>size;
+//     createanddisplay(size);
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+// void maxx(int array[],int n){
+//     int min=0;
+//     int max=0;
+//     for(int i=0;i<=n;i++){
+//         for(int j=i+1;j<=n;j++){
+//             if(array[i]<array[j]){
+//                 min=array[i];
+//             }
+//             else if(array[i]>array[j]){
+//                 max=array[i];
+//             }
+//         }
+//     }
+//     cout<<min<<endl;
+//     cout<<max<<endl;
+//     }
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     int array[n];
+//     for(int i=0;i<=n;i++){
+//         cin>>array[i];
+//     } 
+//     maxx(array,n);
+// }
+
+// int main(){
+//     int array[6]={10,50,30,80,1,0};
+//     int min=0;
+//     int max=0;
+//     for(int i=0;i<=6;i++){
+//         for(int j=i+1;j<=6;j++){
+//             if(array[i]<array[j]){
+//                 min=array[i];
+//             }
+//             else if(array[i]>array[j]){
+//                 max=array[i];
+//             }
+//         }
+//     }
+//     cout<<min<<endl;
+//     cout<<max<<endl;
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// int main(){
+//     //Declaring array by user
+//     int n;
+//     cin>>n;
+//     int array[n];
+//     for(int i=0;i<n;i++){
+//         cin>>array[i];
+//     }
+//     for(int i=0;i<n;i++){
+//         cout<<array[i]<<" ";
+//     }
+
+
+
+
+
+
+    // declaring array by ourself
+    // int array[4]={10,20,30,40};
+    // cout<<array[3];
+//}
