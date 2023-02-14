e0d53eb8759fe4fd154fa0bb6b1d1a85a4d59ae8
Taha Mehboob <110561252+Taha028@users.noreply.github.com>
2023-02-15 02:00:00 +0500

    Add files via upload

---
+++ SquaresOfSortedArray.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[4]={3,4,5,6};
+    int ar=4;
+    for(int i=0;i<ar;i++){
+        cout<<arr[i]*arr[i]<<endl;
+        
+        
+    }
+}
