c656bb9b724b76ab3b2c4e9000163b1a4b57209b
Taha Mehboob <110561252+Taha028@users.noreply.github.com>
2023-02-20 02:03:28 +0500

    Add files via upload

---
+++ 2D Arrays/2D_Array.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+//Searching element in array
+// int main(){
+//     int arr[3][3]={{1,2,3},{4,5,6}};
+//     int x;
+//     cin>>x;
+//     for(int i=0;i<3;i++){
+//         for(int j=0;j<3;j++){
+//             if(arr[i][j]==x){
+//                 cout<<i<<" "<<j<<endl;
+//             }
+
+//         }
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
+
+// int main(){
+//     int r,c;
+//     cin>>r;
+//     cin>>c;
+//     int arr[r][c];
+//     for(int i=0;i<r;i++){
+//         for(int j=0;j<c;j++){
+//             cin>>arr[i][j];
+//         }
+//     }
+//     cout<<"Matrix Ouput:\n";
+//     for(int i=0;i<c;i++){
+//         for(int j=0;j<c;j++){
+//             cout<<arr[i][j]<<" ";
+//         }
+//         cout<<"\n";
+//     }
+// }
+
+// functioon of print 
